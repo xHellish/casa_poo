d7c742d9de1f72ab326bd3a0459eb972f9329b10
Ben Farz <benfarz.bf25@gmail.com>
2023-09-01 09:51:23 -0600

    Nuevos atributos

--- Habitacion/src/Habitacion.java
+++ Habitacion/src/Habitacion.java
@@ -1,5 +1,19 @@
 
 public class Habitacion {
-
-
+	
+	boolean servicios = true;
+	boolean puertaAbierta = true;
+	boolean ventanasAbiertas = true;
+	boolean iluminado = true;
+	boolean sucio = false;
+	boolean taqueado = false;
+	boolean buenEstado = true;
+	
+	int dimensión;
+	int puertasCont;
+	int ventanasCont;
+	
+	String[] objetos = new String[20];
+	
 }
+
